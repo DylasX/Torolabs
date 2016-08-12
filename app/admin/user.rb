1d66d4767d763bb1a9e17a64f16cb2998b92b6d3
Dylas <miguel-sg01@hotmail.com>
2016-08-12 16:39:30 -0500

    Toro-labs

---
+++ app/admin/user.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register User do
+
+  permit_params :name, :phone, :email
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  index do
+    column :name
+    column :phone
+    column :email
+
+  end
+
+  filter :name
+  filter :phone
+  filter :email
+
+
+end
