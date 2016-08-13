bbddf198660a44f38a790bbac7992b2425225af3
Dylas <miguel-sg01@hotmail.com>
2016-08-12 20:28:44 -0500

    Yeo

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root "user#index"
+  root "users#index"
   resources :users
    #root "admin/dashboard#index"
 
